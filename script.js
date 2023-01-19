a0c06ba6e2e6208eafdfe8b2b9f83d4be7095cc1
LucasJSM <lucasjsm2001@gmail.com>
2023-01-19 15:55:12 -0300

    clean js

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const form = document.querySelector(".form-habits")
 const nlwSetup = new NLWSetup(form)
 
-const habits = document.querySelector(`.habits`)
-
 const data = {
   run: ["01-01", "01-02", "01-03", "01-06"],
   water: ["01-01", "01-02", "01-03", "01-04"],
   food: ["01-01", "01-02"],
-  gym: ["01-03", "01-04"],
+  gym: ["01-01", "01-04"],
+  sleep: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06"],
 }
 
 nlwSetup.setData(data)
